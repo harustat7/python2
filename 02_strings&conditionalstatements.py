d20bbab4e42b0dc79bb8f43fa98c21d7196ff3f4
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-04-27 20:50:51 +0000

    strings

---
+++ 02_strings&conditionalstatements.py
@@ -0,0 +1,30 @@
+
+#strings-strings is a type of datatype that stores the sequence of characters
+#str1="this is a string"
+#str2='this is a string'
+#str3=this is a string"""
+
+#if we use single quotes in a string then python will be confused 
+#'this is apnacollege's tutorial'
+#"this is apnacollege's tutorial"
+
+#str4="this is a string.we are learing strings in python"
+
+#escape sequence characters-\n(newline)
+
+#str5="this is a string.\n we are learning strings"
+#print(str5)
+
+"""
+#basic operations
+#concatenation
+
+str1="apna"
+str2="college"
+print(str1+" "+str2)
+"""
+
+#length of string
+str="hello"
+len1=len(str)
+print(len1)
