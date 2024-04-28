1464399f1f14ba9767d670fc460c647eeb857ddd
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-04-28 18:20:42 +0000

    loops

--- 05-loops.py
+++ 05-loops.py
@@ -20,13 +20,31 @@ j=5
 while j>=1:
     print(j)
     j-=1
-"""
+
 #print the multiplication table of number n
 i=1
 n=int(input("num: "))
-while n<=10:
+while i<=10:
     j=n*i;
     print(j)
     i+=1
 
+"""
 
+#print the square of elements
+i=1
+while i<=10:
+    j=i*i
+    print(j)
+    i+=1
+
+#search for a number x in this tuple using loop
+tup=(1,4,9,16,25,36,49,64,81,100)
+x=int(input("x: "))
+i=0
+while i<len(tup):
+    if(x==tup[i]):
+        print("x found",x)
+    else:
+        print("x not found") 
+    i+=1
