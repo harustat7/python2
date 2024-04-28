815cf7748be24728d0a34c0f8d3c2b53f0366daa
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-04-28 18:02:48 +0000

    loops

---
+++ 05-loops.py
@@ -0,0 +1,11 @@
+"""
+loops
+loops are used to repeat instructions
+they are of two types(while and for)
+while condition:
+    statement
+"""
+count=1
+while count<=5:
+    print("hello")
+    count=count+1
